a4c7149f5136de9f4f34421b5f02e584bb4b1752
Nil Panés <nil.panes@gmail.com>
2017-11-20 16:16:59 +0100

    generated service, get items

---
+++ src/app/items.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ItemsService {
+
+  private url = 'http://localhost:3000';  
+
+  constructor(private http: HttpClient) { }
+
+  public getItems(page: number): Observable<Object> {
+    const url = `${this.url}/items?_page=${page}&_limit=5`;
+    return this.http.get(url);
+  }
+
+}
